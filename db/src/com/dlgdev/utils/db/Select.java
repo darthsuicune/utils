935f23d88260e8518225e5ec6f48c536f1958ce1
Denis Lapuente <darthsuicune@gmail.com>
2016-09-10 21:46:38 +0200

    Start work on database utils

---
+++ db/src/com/dlgdev/utils/db/Select.java
@@ -0,0 +1,53 @@
+package com.dlgdev.utils.db;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+
+import javax.sql.DataSource;
+
+public class Select {
+	private DataSource dataSource;
+	private StringBuilder sql;
+	private String[] whereArgs;
+
+	public Select(DataSource dataSource) {
+		this.dataSource = dataSource;
+		sql = new StringBuilder("SELECT *");
+	}
+
+	public Select(DataSource dataSource, String[] columns) {
+		this.dataSource = dataSource;
+		sql = new StringBuilder("SELECT " + Arrays.stream(columns).collect(
+				Collectors.joining(",")));
+	}
+
+	public Select from(String tableName) {
+		sql.append(" FROM " ).append(tableName);
+		return this;
+	}
+	public Select where(String where, String[] whereArgs) {
+		sql.append(" WHERE ").append(where);
+		this.whereArgs = whereArgs;
+		return this;
+	}
+
+	public void execute(Consumer<ResultSet> consumer) {
+		try(Connection connection = dataSource.getConnection()) {
+			PreparedStatement statement = connection.prepareStatement(sql.toString());
+			ResultSet set = statement.executeQuery();
+			if(set != null) {
+				consumer.accept(set);
+			} else {
+				throw new RuntimeException("Error while running the query: " + sql.toString());
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new RuntimeException("Error while running the query: " + sql.toString(), e);
+		}
+	}
+}
---
+++ db/test/com/dlgdev/utils/db/SelectTest.java
@@ -0,0 +1,55 @@
+package com.dlgdev.utils.db;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class SelectTest {
+	@Mock DataSource source;
+	@Mock Connection connection;
+	@Mock PreparedStatement statement;
+	@Mock ResultSet resultSet;
+
+	@Before public void setUp() throws Exception {
+		MockitoAnnotations.initMocks(this);
+
+		when(source.getConnection()).thenReturn(connection);
+		when(connection.prepareStatement(anyString())).thenReturn(statement);
+		doNothing().when(statement).setString(anyInt(), anyString());
+		when(statement.executeQuery()).thenReturn(resultSet);
+	}
+
+	@Test public void testFromWithoutConditionsIsCalled() throws Exception {
+		new Select(source).from("a").execute(this::verifySet);
+		verify(connection).prepareStatement("SELECT * FROM a");
+		verify(statement).executeQuery();
+	}
+
+	private void verifySet(ResultSet set) {
+		assertEquals(set, resultSet);
+	}
+
+	@Test public void testWithWhereMakesARestrictedSearch() throws SQLException {
+		String[] whereArgs = {"1"};
+		new Select(source).from("a").where("a=?", whereArgs).execute(this::verifySet);
+		verify(connection).prepareStatement("SELECT * FROM a WHERE a=?");
+		verify(statement, times(whereArgs.length)).setString(anyInt(), anyString());
+		verify(statement).executeQuery();
+	}
+}
