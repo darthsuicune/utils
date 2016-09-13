3abc3443c6a508c1c5d37816584a6e229e64a8a0
Denis Lapuente <darthsuicune@gmail.com>
2016-09-13 20:32:17 +0200

    Implement insert tests

--- db/test/com/dlgdev/utils/db/InsertTest.java
+++ db/test/com/dlgdev/utils/db/InsertTest.java
@@ -1,6 +1,45 @@
 package com.dlgdev.utils.db;
 
+import com.dlgdev.utils.db.exceptions.MalformedSqlException;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.verify;
+
 public class InsertTest extends SqlTest{
+	@Test public void emptyTableThrowsException() throws Exception {
+		try {
+			Insert.into(source, "");
+		} catch (MalformedSqlException e) {
+			assertNotNull(e);
+		}
+	}
 
+	@Test public void insertSingleValueSet() throws Exception {
+		Map<String, String> values = new HashMap<>(2);
+		values.put("b", "d");
+		values.put("c", "e");
+		Insert.into(source, "a").values(values).run();
+		verify(connection).prepareStatement("INSERT INTO a(b,c) VALUES (d,e)");
+	}
 
+	@Test public void insertValues() throws Exception {
+		Collection<Map<String, String>> list = new ArrayList<>(2);
+		Map<String, String> values = new HashMap<>(2);
+		values.put("b", "d");
+		values.put("c", "e");
+		list.add(values);
+		values = new HashMap<>(2);
+		values.put("b", "f");
+		values.put("c", "g");
+		list.add(values);
+		Insert.into(source, "a").values(list).run();
+		verify(connection).prepareStatement("INSERT INTO a(b,c) VALUES (d,e),(f,g)");
+	}
 }
