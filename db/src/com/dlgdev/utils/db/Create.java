14317b2a7ac5ff545b04ff6254b92dfad7f4b1cd
Denis Lapuente <lapuente@abbyy.com>
2016-09-15 18:20:17 +0200

    Add and test Create.

--- db/src/com/dlgdev/utils/db/Create.java
+++ db/src/com/dlgdev/utils/db/Create.java
@@ -1,4 +1,134 @@
 package com.dlgdev.utils.db;
 
+import com.dlgdev.utils.db.exceptions.MalformedSqlException;
+import org.apache.commons.lang3.StringUtils;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class Create {
+	private DataSource source;
+
+	private Create(DataSource source) {
+		this.source = source;
+	}
+
+	public static Create in(DataSource source) {
+		return new Create(source);
+	}
+
+	public TableCreator table(String tableName) {
+		if (StringUtils.isEmpty(tableName)) {
+			throw new MalformedSqlException("You cannot create a table with an empty name");
+		}
+		return new TableCreator(source, tableName);
+	}
+
+	public static class TableCreator {
+		private DataSource source;
+		private String tableName;
+		private Map<String, String> fields = new HashMap<>();
+		private String primaryKey;
+		private List<ForeignKey> foreignKeys = new ArrayList<>();
+		private List<String> indices = new ArrayList<>();
+
+		private TableCreator(DataSource source, String tableName) {
+			this.source = source;
+			this.tableName = tableName;
+		}
+
+
+		public TableCreator fields(Map<String, String> fields) {
+			if(fields.isEmpty()) {
+				throw new MalformedSqlException("You cannot create a table without fields.");
+			}
+			this.fields.putAll(fields);
+			return this;
+		}
+
+		public TableCreator primaryKey(String key) {
+			this.primaryKey = key;
+			return this;
+		}
+
+		public TableCreator primaryKey(List<String> key) {
+			this.primaryKey = key.stream().collect(Collectors.joining(","));
+			return this;
+		}
+
+		public TableCreator foreignKey(ForeignKey foreignKey) {
+			this.foreignKeys.add(foreignKey);
+			return this;
+		}
+
+		public TableCreator foreignKeys(List<ForeignKey> foreignKeys) {
+			this.foreignKeys.addAll(foreignKeys);
+			return this;
+		}
+
+		public TableCreator indices(List<String> indices) {
+			this.indices = indices;
+			return this;
+		}
+
+		public void run() {
+			String sql = String.format("CREATE TABLE %s (%s%s%s)", tableName, fieldsAsString(), primaryKeyAsString(),
+					foreignKeysAsString());
+			try (Connection connection = source.getConnection()) {
+				connection.prepareStatement(sql).execute();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+
+		private String primaryKeyAsString() {
+			if(StringUtils.isNotEmpty(primaryKey)) {
+				return String.format(", PRIMARY KEY (%s)", primaryKey);
+			} else {
+				return "";
+			}
+		}
+
+		private String fieldsAsString() {
+			if(fields.isEmpty()) {
+				throw new MalformedSqlException("You cannot create a table without fields.");
+			}
+			StringBuilder builder = new StringBuilder();
+			fields.forEach((key, value) -> builder.append(key).append(" ").append(value).append(", "));
+			return builder.delete(builder.length() - 2, builder.length()).toString();
+		}
+
+		private String foreignKeysAsString() {
+			if(foreignKeys.isEmpty()) {
+				return "";
+			}
+			StringBuilder builder = new StringBuilder(", ");
+			foreignKeys.forEach(
+					(key) -> builder.append(String.format("CONSTRAINT %s FOREIGN KEY (%s) REFERENCES %s (%s)",
+							key.constraintName(tableName), key.localField, key.referencedTable, key.referencedField)));
+			return builder.toString();
+		}
+	}
+
+	public static class ForeignKey {
+		public String localField;
+		public String referencedTable;
+		public String referencedField;
+
+		public ForeignKey(String localField, String referencedTable, String referencedField) {
+			this.localField = localField;
+			this.referencedTable = referencedTable;
+			this.referencedField = referencedField;
+		}
+
+		public String constraintName(String tableName) {
+			return String.format("`%s_%s_%s_%s`", tableName, localField, referencedTable, referencedField);
+		}
+	}
 }
---
+++ db/test/com/dlgdev/utils/db/CreateTest.java
@@ -0,0 +1,71 @@
+package com.dlgdev.utils.db;
+
+import com.dlgdev.utils.db.exceptions.MalformedSqlException;
+import org.junit.Test;
+
+import java.sql.SQLException;
+import java.util.*;
+
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.verify;
+
+public class CreateTest extends SqlTest {
+	String tableName = "a";
+	Map<String, String> fields = new HashMap<>();
+	String key = "b";
+	List<Create.ForeignKey> foreignKeys = new ArrayList<>();
+	List<String> indices = new ArrayList<>();
+
+	@Test public void comprehensiveApiIsAvailable() {
+		fields.put("a", "b");
+		Create.in(source)
+				.table(tableName)
+				.fields(fields)
+				.primaryKey(key)
+				.foreignKeys(foreignKeys)
+				.indices(indices).run();
+	}
+
+	@Test public void emptyTableNameThrowsException() {
+		try {
+			Create.in(source).table("");
+		} catch (MalformedSqlException e) {
+			assertNotNull(e);
+		}
+	}
+
+	@Test public void emptyFieldsThrowsException() {
+		try {
+			Create.in(source).table("a").fields(fields).run();
+		} catch (MalformedSqlException e) {
+			assertNotNull(e);
+		}
+	}
+
+	@Test public void createsAProperSqlSentence() throws SQLException {
+		fields.put("b", "c");
+		fields.put("d", "e");
+		Create.in(source).table("a").fields(fields).run();
+		verify(connection).prepareStatement("CREATE TABLE a (d e, b c)");
+	}
+
+	@Test public void withPrimaryKey() throws Exception {
+		fields.put("a", "b");
+		Create.in(source).table("a").fields(fields).primaryKey("a").run();
+		verify(connection).prepareStatement("CREATE TABLE a (a b, PRIMARY KEY (a))");
+	}
+
+	@Test public void withCompositePrimaryKey() throws Exception {
+		fields.put("a", "b");
+		fields.put("c", "b");
+		Create.in(source).table("a").fields(fields).primaryKey(Arrays.asList("a", "c")).run();
+		verify(connection).prepareStatement("CREATE TABLE a (a b, c b, PRIMARY KEY (a,c))");
+	}
+
+	@Test public void withForeignKey() throws Exception {
+		fields.put("a", "b");
+		Create.in(source).table("a").fields(fields).foreignKey(new Create.ForeignKey("a", "h", "j")).run();
+		verify(connection).prepareStatement("CREATE TABLE a (a b, " +
+				"CONSTRAINT `a_a_h_j` FOREIGN KEY (a) REFERENCES h (j))");
+	}
+}
