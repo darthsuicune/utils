932456a34591fe88d3b0070c04e0a490df87155b
Denis Lapuente <denis.lapuente@jetbrains.com>
2017-01-09 11:49:36 +0100

    Add stubs for mysql Select and its test class.

---
+++ db/src/main/kotlin/com/dlgdev/utils/db/mysql/Select.kt
@@ -0,0 +1,10 @@
+package com.dlgdev.utils.db.mysql
+
+import com.dlgdev.utils.db.Select
+import javax.sql.DataSource
+
+class Select(val dataSource: DataSource): Select {
+    fun from(table: String) {
+
+    }
+}
---
+++ db/src/test/kotlin/com/dlgdev/utils/db/mysql/SelectTest.kt
@@ -0,0 +1,13 @@
+package com.dlgdev.utils.db.mysql
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class SelectTest {
+    @Test
+    fun from() {
+
+    }
+
+}
