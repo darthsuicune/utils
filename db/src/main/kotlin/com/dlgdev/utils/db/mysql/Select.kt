384a967cebd03f777dbe06e0778f5433ef94a7a2
Denis Lapuente <denis.lapuente@jetbrains.com>
2017-01-12 12:43:02 +0100

    Implement mysql Select

--- db/src/main/kotlin/com/dlgdev/utils/db/mysql/Select.kt
+++ db/src/main/kotlin/com/dlgdev/utils/db/mysql/Select.kt
@@ -1,17 +1,36 @@
 package com.dlgdev.utils.db.mysql
 
-import com.dlgdev.utils.db.From
 import com.dlgdev.utils.db.Select
+import com.dlgdev.utils.db.Where
+import java.sql.ResultSet
+import java.util.function.Function
 import javax.sql.DataSource
 
 class Select(val dataSource: DataSource): Select {
+    val sql = StringBuilder("SELECT ")
+    var columns: Array<out String> = emptyArray()
+
     constructor(dataSource: DataSource, vararg columns: String) : this(dataSource) {
         this.columns = columns
     }
 
-    var columns: Array<out String> = emptyArray()
+    override fun from(tableName: String): com.dlgdev.utils.db.From {
+        if(columns.isEmpty()) {
+            sql.append("*")
+        } else {
+            sql.append(columns.joinToString(","))
+        }
+        sql.append(" FROM $tableName")
+        return From(sql)
+    }
 
-    override fun from(tableName: String): From {
-        throw UnsupportedOperationException("not implemented") //To change body of created functions use File | Settings | File Templates.
+    class From(val sql: StringBuilder) : com.dlgdev.utils.db.From {
+        override fun where(where: String, whereArgs: Array<String>): Where {
+            throw UnsupportedOperationException("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+        override fun <T> apply(function: Function<ResultSet, T>): T {
+            throw UnsupportedOperationException("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+
     }
 }
